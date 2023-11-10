008a41b8c5eaf33546ad2f2afe5763eb8398de74
St3v3nA <kristo-steven.altmae@ametikool.ee>
2023-11-10 11:42:15 +0200

    initial commit

---
+++ src/app/components/burger.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const burgerMenu = () => {
+return (
+  <div>
+    <div id="burger-menu">
+      <span></span>
+    </div>
+
+    <div id="menu">
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+  </div>
+  );
+};
+
+export default burgerMenu;
